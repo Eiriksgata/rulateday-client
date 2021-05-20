4222923263d5bec92569071cfd9737bf9e739d24
Eiriksgata <2353686862@qq.com>
2021-05-20 09:21:12 +0800

    Update error page router config

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
+// error page components
+const PAGE_403 = () => import('@/views/error/Page403.vue');
+const PAGE_404 = () => import('@/views/error/Page404.vue');
+const PAGE_500 = () => import('@/views/error/Page500.vue');
+
+const HOME = () => import('@/views/Home.vue');
+
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+
+  // error pages
+  { path: '/error/403', name: 'page403', component: PAGE_403 },
+  { path: '/error/404', name: 'page404', component: PAGE_404 },
+  { path: '/error/500', name: 'page500', component: PAGE_500 },
+
+  //home
+  { path: '/', name: 'Home', component: HOME }
+
+
 ]
 
 const router = new VueRouter({
