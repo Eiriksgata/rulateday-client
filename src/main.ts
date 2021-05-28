728496384be5d27178712412e3067010b0a707aa
Eiriksgata <2353686862@qq.com>
2021-05-28 15:05:11 +0800

    Add axios config

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './plugins/element.js'
+import './plugins/element.ts'
 import i18n from './i18n'
 
 import moment from 'moment'//导入文件
