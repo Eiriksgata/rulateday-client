66080a701acf7cdd3fa40433e70775efbcd3076e
Eiriksgata <2353686862@qq.com>
2021-05-28 15:04:43 +0800

    Update router config

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,19 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
+const HOME = () => import('@/views/navar/Home.vue');
+
+//exception views
+const EXCEPTION_COMMIT = () => import('@/views/feedback/CommitViews.vue');
+
+const EXCEPTION_LIST = () => import('@/views/feedback/StatisticsList.vue');
+
 // error page components
 const PAGE_403 = () => import('@/views/error/Page403.vue');
 const PAGE_404 = () => import('@/views/error/Page404.vue');
 const PAGE_500 = () => import('@/views/error/Page500.vue');
 
-const HOME = () => import('@/views/Home.vue');
+
 
 const routes = [
 
@@ -18,8 +25,12 @@ const routes = [
   { path: '/error/500', name: 'page500', component: PAGE_500 },
 
   //home
-  { path: '/', name: 'Home', component: HOME }
+  { path: '/', name: 'Home', component: HOME },
 
+  //
+  { path: '/exception/commit', name: 'ExceptionCommit', component: EXCEPTION_COMMIT },
+  { path: '/exception/list', name: 'ExceptionList', component: EXCEPTION_LIST },
+  
 
 ]
 
